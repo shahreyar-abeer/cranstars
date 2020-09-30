b8abe9c9a0281cc65a1e24d323566e887effa31c
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-09-30 09:38:14 +0600

    added data, all checks passed

---
+++ R/cran_dl_data.R
@@ -0,0 +1,12 @@
+
+#' CRAN downloads for 20 tidyverse packages
+#'
+#' A dataset containing the daily number of downloads for the supplied packages
+#'
+#' @format A data frame with 77940 rows and 3 variables:
+#' \describe{
+#'   \item{date}{The date}
+#'   \item{count}{Number of downlaods in the date}
+#'   \item{package}{Name of package}
+#' }
+"cran_dl_data"
---
+++ R/gh_stars_data.R
@@ -0,0 +1,13 @@
+
+
+#' Github Stars for 20 tidyverse packages
+#'
+#' A dataset containing the daily number of stars the packages got
+#'
+#' @format A data frame with 12748 rows and 3 variables:
+#' \describe{
+#'   \item{date}{The date}
+#'   \item{n_stars}{Number of stars in the date}
+#'   \item{package}{Repo of package}
+#' }
+"gh_stars_data"
