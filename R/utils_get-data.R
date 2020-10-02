cafef5c1cc9cf665c01731a9077785a08c3e3808
shahreyar-abeer <shahreyar.abeer@gmail.com>
2020-10-02 09:56:31 +0600

    util functions for plotting

--- R/utils_get-data.R
+++ R/utils_get-data.R
@@ -12,3 +12,14 @@ get_cran_name <- function(repo) {
   broken <- strsplit(repo, "/")
   unlist(lapply(broken, "[[", 2))
 }
+
+orange <- "#DA7C30"
+
+
+adjectives <- c("outstanding", "great", "first-class", "awesome", "luminous",
+                "extraordinary", "polished", "stylish", "magnificent", "stunning",
+                "superb", "praiseworthy", "marvelous", "sensational", "remarkable")
+
+make_yrmon <- function(date) {
+  as.Date(paste0(format(date, "%Y-%m"), "-01"))
+}
